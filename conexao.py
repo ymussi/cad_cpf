7e3705c61cd886a91401889a0c27dc97f69ca3eb
Yuri <46067335+ymussi@users.noreply.github.com>
2019-01-11 23:51:19 -0200

    Update conexao.py

--- conexao.py
+++ conexao.py
@@ -1,14 +1,5 @@
 import MySQLdb
 
-
-def conexao_db_ext():
-    connection = MySQLdb.connect(user='casedecom_tomatico', passwd='tomatico', db='casedecom_cpf', host='185.201.10.1')
-    return connection
-
-    connection.close()
-
-
-
 def conexao_db():
     connection = MySQLdb.connect(user='task', passwd='task', db='cpf2', host='localhost')
     return connection
